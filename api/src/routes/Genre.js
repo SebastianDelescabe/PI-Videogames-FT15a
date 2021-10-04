26945c9808bc1a27d21da6c15ac65823fa432243
SebastianDelescabe <sebastiandelescabe@gmail.com>
2021-10-04 17:08:59 -0300

    database heroku

--- api/src/routes/Genre.js
+++ api/src/routes/Genre.js
@@ -4,15 +4,38 @@ const { Genre } = require('../db');
 
 const router = Router();
 
-router.get("/genre", async function (req, res) {
-    Genre.findAll().then((response) => {
-        if(response){
-            res.send(response)
-        }else{
-            res.status(404)
+const apiGenre = async function () {
+    const info = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.API_KEY}`)
+    const apiGenre = info.data.results.map(e => {
+        return {
+            name: e.name,
         }
     })
+    return apiGenre
+}
+router.get("/genre", async function (req, res) {  
+
+    const genre = await apiGenre()
+
+    if(genre){
+        genre.map(e => Genre.findOrCreate({
+            where:{
+                name:e.name
+            }
+        }))
+    }
+    res.send(genre)
 })
+
+// router.get("/genre", async function (req, res) {
+//     Genre.findAll().then((response) => {
+//         if(response){
+//             res.send(response)
+//         }else{
+//             res.status(404)
+//         }
+//     })
+// })
 
 module.exports = router;
 
