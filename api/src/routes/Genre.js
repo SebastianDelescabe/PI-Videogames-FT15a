57657fe800a3ce0eb77daf67ea5963ab8f76ae8a
SebastianDelescabe <sebastiandelescabe@gmail.com>
2021-10-04 17:24:45 -0300

    heroku deploy

--- api/src/routes/Genre.js
+++ api/src/routes/Genre.js
@@ -4,38 +4,16 @@ const { Genre } = require('../db');
 
 const router = Router();
 
-const apiGenre = async function () {
-    const info = await axios.get(`https://api.rawg.io/api/genres?key=${process.env.API_KEY}`)
-    const apiGenre = info.data.results.map(e => {
-        return {
-            name: e.name,
+
+router.get("/genre", async function (req, res) {
+    Genre.findAll().then((response) => {
+        if(response){
+            res.send(response)
+        }else{
+            res.status(404)
         }
     })
-    return apiGenre
-}
-router.get("/genre", async function (req, res) {  
-
-    const genre = await apiGenre()
-
-    if(genre){
-        genre.map(e => Genre.findOrCreate({
-            where:{
-                name:e.name
-            }
-        }))
-    }
-    res.send(genre)
 })
-
-// router.get("/genre", async function (req, res) {
-//     Genre.findAll().then((response) => {
-//         if(response){
-//             res.send(response)
-//         }else{
-//             res.status(404)
-//         }
-//     })
-// })
 
 module.exports = router;
 
--- api/src/routes/Plarforms.js
+++ api/src/routes/Plarforms.js
@@ -4,65 +4,15 @@ const { Platforms } = require('../db');
 
 const router = Router();
 
-const apiInfo = async function () {
-    let allGames = []
-    let promises = []
-
-    try {
-        for (let i = 1; i <= 5; i++) {
-            promises.push(axios.get(`https://api.rawg.io/api/games?key=${process.env.API_KEY}&page=${i}`)
-                .then(response => {
-                    return response
-                }))
-
+router.get("/platforms",function (req, res) {  
+    Platforms.findAll().then((response) => {
+        if(response){
+            res.send(response)
+        }else{
+            res.status(404)
         }
-        await Promise.all(promises)
-            .then((response) => {
-                for (let i = 0; i < promises.length; i++) {
-                    allGames = allGames.concat(response[i].data.results.map(e => {
-                        return {
-                            platforms: e.platforms.map(e => {
-                                return {
-                                    name: e.platform.name
-                                }
-                            }),
-                        }
-                    }))
-                }
-            })
-        return allGames
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-router.get("/platforms", async function (req, res) {  
-    const plat = await apiInfo()
-    const plat2 = plat.map(e => e.platforms).flat()
-    const plat3 = plat2.map(e => e.name)
-    const trim = plat3.map(e => e.trim())
-    const noRepeatedTemp = [...new Set(trim)]
-
-    noRepeatedTemp.map((e) => {
-        Platforms.findOrCreate({
-            where:{
-                name:e
-            }
-        })
     })
-    const hola =  await Platforms.findAll()
-    res.send(hola) 
 })
-
-// router.get("/platforms",function (req, res) {  
-//     Platforms.findAll().then((response) => {
-//         if(response){
-//             res.send(response)
-//         }else{
-//             res.status(404)
-//         }
-//     })
-// })
 
 
 module.exports = router;
