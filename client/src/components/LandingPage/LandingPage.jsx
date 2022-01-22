113797cafa3826a6b891ff8fdd9f273dbafc17f8
Sebastian <sebastiandelescabe@gmail.com>
2022-01-22 19:59:55 -0300

    about

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,10 +5,17 @@ import styledLanding from './LandingPage.module.css'
 export default function LandingPage() {
     return (
         <div className={styledLanding.background}>
-            <div className= {styledLanding.flex}>
-                <Link to="/home">
-                    <button className = {styledLanding.btn}>START</button>
-                </Link>
+            <div className={styledLanding.flex}>
+                <h1 className={styledLanding.h1} >ABOUT</h1>
+                <span className={styledLanding.span}>Developer:  Sebastian Delescabe</span>
+                <span className={styledLanding.span}>Purpose:  Project in Bootcamp SoyHenry!!</span>
+                <span className={styledLanding.span}>Programming Language:  JavaScript</span>
+                <span className={styledLanding.span}>Techs:  React - Redux - NodeJs - CSS - PostgreSQL - Sequelize - Express </span>
+            </div>
+            <div>
+            <Link to="/home">
+                <button className={styledLanding.btn}>START</button>
+            </Link>
             </div>
         </div>
     )
